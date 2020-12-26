2bb0c812741e04a1ab2442a81026e1fea7e1aebb
arfarells <k.jjones8588@student.valleycollege.edu>
2020-12-26 21:57:28 +0700

    3

--- express/server.js
+++ express/server.js
@@ -15,19 +15,21 @@ function customHeaders(req, res, next) {
 	res.setHeader('X-Powered-By', 'ROPEL ID');
 	next();
 }
+
+router.get('/', (req, res) => {
+	res.send(`<h1>Running at ${port}</h1>`);
+});
+router.get('/dok', (req, res) => {
+	res.send({ ok: 'ok' });
+});
+router.get('*', function(req, res) {
+	res.status(404).send('<h1>404</h1>');
+});
+
 app.use('/.netlify/functions/server', router); // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 app.use(customHeaders);
 app.use(express.json());
-app.get('/', (req, res) => {
-	res.send(`<h1>Running at ${port}</h1>`);
-});
-app.get('/dok', (req, res) => {
-	res.send({ ok: 'ok' });
-});
-app.get('*', function(req, res) {
-	res.status(404).send('<h1>404</h1>');
-});
 
 module.exports = app;
 module.exports.handler = serverless(app);
