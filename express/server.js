46770f05126940f99520d0f96b3bca05c1026f07
arfarells <k.jjones8588@student.valleycollege.edu>
2020-12-26 23:43:31 +0700

    2a

--- express/server.js
+++ express/server.js
@@ -4,29 +4,16 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const { query } = require('../config');
-
-// const bodyParser = require('body-parser');
 const cors = require('cors');
-// const akun = require('../routers/akun');
-// const Profile = require('../routers/Profile');
-// const alamat = require('../routers/alamat');
-// const Laporan = require('../routers/Laporan');
-// const Admin = require('../routers/admin/admin');
 const router = express.Router();
 app.use(cors());
-
 app.enable('trust proxy');
 function customHeaders(req, res, next) {
 	app.disable('x-powered-by');
 	res.setHeader('X-Powered-By', 'ROPEL ID');
 	next();
 }
-// app.use(alamat);
-// app.use(akun);
-// app.use(Profile);
-// app.use(Laporan);
-// app.use(Admin);
-// app.use('/uploads', express.static('uploads'));
+
 router.get('/Provinsi', async (req, res) => {
 	var sql = `SELECT id, name as prov FROM m_prov`;
 	try {
@@ -45,7 +32,7 @@ router.get('/Provinsi', async (req, res) => {
 	}
 });
 router.get('/', (req, res) => {
-	res.send(`<h1>Running at ${'999'}</h1>`);
+	res.send(`<h1>Running at 3003</h1>`);
 });
 router.get('/dok', (req, res) => {
 	res.send({ ok: 'ok' });
