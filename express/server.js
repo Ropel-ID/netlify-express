c0e4f12e39b54585a16d4a9107b2da66c4a33f93
arfarells <k.jjones8588@student.valleycollege.edu>
2020-12-26 23:25:19 +0700

    a

--- express/server.js
+++ express/server.js
@@ -3,13 +3,15 @@ const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
+const { query } = require('../config');
+
 // const bodyParser = require('body-parser');
 const cors = require('cors');
-const akun = require('../routers/akun');
-const Profile = require('../routers/Profile');
-const alamat = require('../routers/alamat');
-const Laporan = require('../routers/Laporan');
-const Admin = require('../routers/admin/admin');
+// const akun = require('../routers/akun');
+// const Profile = require('../routers/Profile');
+// const alamat = require('../routers/alamat');
+// const Laporan = require('../routers/Laporan');
+// const Admin = require('../routers/admin/admin');
 const router = express.Router();
 app.use(cors());
 
@@ -25,6 +27,23 @@ function customHeaders(req, res, next) {
 // app.use(Laporan);
 // app.use(Admin);
 // app.use('/uploads', express.static('uploads'));
+router.get('/Provinsi', async (req, res) => {
+	var sql = `SELECT id, name as prov FROM m_prov`;
+	try {
+		const result = await query(sql);
+
+		return res.send({
+			success: true,
+			message: 'User Tidak terdafatar',
+			data: result
+		});
+	} catch (err) {
+		return res.send({
+			success: false,
+			message: err.message
+		});
+	}
+});
 router.get('/', (req, res) => {
 	res.send(`<h1>Running at ${'999'}</h1>`);
 });
@@ -35,7 +54,7 @@ router.get('*', function(req, res) {
 	res.status(404).send('<h1>404</h1>');
 });
 
-app.use(router); // path must route to lambda
+app.use('/.netlify/functions/server', router); // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 app.use(customHeaders);
 app.use(express.json());
